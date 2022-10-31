078d58bba7300d225e4192c4314218eb6f11d723
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2022-10-31 14:12:56 -0400

    Add slider that changes dimensions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 const container = document.querySelector('.grid-container')
+const color = document.querySelector('#box-color')
+const clearBtn = document.querySelector('#clear')
+const slider = document.querySelector('#slider')
+const output = document.querySelector("#dimensions")
 
-function makeDivs(divs){
+
+slider.oninput = function() {
+    output.innerHTML = `${this.value} x ${this.value}`;
+}
+
+
+
+function makeDivs(divs = Number(slider.value)){
     let dimensions = divs**2
     for (let i = 0; i <= dimensions -1; i++ ){
         const box = document.createElement('div')
@@ -11,26 +22,51 @@ function makeDivs(divs){
     container.style.gridTemplateColumns = `repeat(${divs}, 1fr)`;
 };
 
-
-function events(){
-    container.addEventListener('mousemove', function(e){
-        if (e.buttons == 1){
-            changeBoxColor(e)
-        }});
-    container.addEventListener('click', changeBoxColor);
-}
-
-
 function changeBoxColor(e){
-    e.target.style.background = 'gray';
+    e.target.style.background = `${color.value}`;
     e.target.classList.add('colored');
     container.style.removeProperty('background');
     
 };
 
-makeDivs(16);
+function clearEverything(){
+    try
+    {
+    for (let child in container.children){
+    container.children[child].removeAttribute('style')
+    container.children[child].classList.remove('colored')
+    }
+    }  
+    catch(err){} 
+}
+             
+
+function events(){
+    container.addEventListener('mousemove', function(e){
+        if (e.buttons == 1){
+            changeBoxColor(e)
+            e.preventDefault()
+        }});
+    container.addEventListener('click', changeBoxColor);
+    clearBtn.addEventListener('click', () => {clearEverything()});
+    slider.addEventListener('input', () => {
+        container.replaceChildren();
+        makeDivs()})
+}
+
+
+makeDivs()
 
 events()
+
+
+
+
+
+
+
+
+console.log(typeof(Number(slider.value)))
 
 
 

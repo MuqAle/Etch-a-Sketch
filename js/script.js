3abeb2849899b1e2582920a8327f5cb82e4b9888
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2022-11-02 13:40:03 -0400

    Create function that changes font clr when clicked

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-const container = document.querySelector('.grid-container')
-const color = document.querySelector('#box-color')
-const clearBtn = document.querySelector('#clear')
-const slider = document.querySelector('#slider')
-const output = document.querySelector("#dimensions")
-const eraserBtn = document.querySelector("#eraser")
-const penBtn = document.querySelector("#pen")
-const randomBtn = document.querySelector('#random-color')
+const container = document.querySelector('.grid-container');
+const color = document.querySelector('#box-color');
+const clearBtn = document.querySelector('#clear');
+const slider = document.querySelector('#slider');
+const output = document.querySelector("#dimensions");
+const eraserBtn = document.querySelector("#eraser");
+const penBtn = document.querySelector("#pen");
+const randomBtn = document.querySelector('#random-color');
+const buttons = document.querySelectorAll(".button-categories");
+
 
 
 
 slider.oninput = function() {
     output.innerHTML = `${this.value} x ${this.value}`;
-}
+};
 
 
 
@@ -35,7 +37,7 @@ function changeBoxColor(e){
 
 function randomNumber(){
     return Math.floor(Math.random() * 256)
-}
+};
 
 
 function randomColor(e){
@@ -43,73 +45,93 @@ function randomColor(e){
     e.target.style.background = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`
     e.target.classList.add('colored');
     container.style.removeProperty('background');
-}
+};
 
 function clearEverything(){
     try
     {
     for (let child in container.children){
-    container.children[child].removeAttribute('style')
-    container.children[child].classList.remove('colored')
-    }
-    }  
-    catch(err){} 
-}
+    container.children[child].removeAttribute('style');
+    container.children[child].classList.remove('colored');
+    };
+    } 
+    catch(err){}; 
+};
 
 function eraseColor(e){
 
-    e.target.style.removeProperty('background')
-    e.target.classList.remove('colored')
+    e.target.style.removeProperty('background');
+    e.target.classList.remove('colored');
 }
 
 
 function usingPen(){
     
-        container.addEventListener('mousemove', function(e){
-            if (e.buttons == 1){
-                changeBoxColor(e);
-                e.preventDefault();
-            }});
-        container.addEventListener('click', changeBoxColor);
-    }
+     container.addEventListener('mousemove', function(e){
+        if (e.buttons == 1){
+            changeBoxColor(e);
+            e.preventDefault();
+        }});
+    container.addEventListener('click', changeBoxColor);
+};
 
 
 function usingEraser(){
 
-        container.addEventListener('mousemove', function(e){
-            if (e.buttons == 1){
-                eraseColor(e);
-                e.preventDefault()
-            }
-        });
-        container.addEventListener('click', eraseColor)
-    }
+    container.addEventListener('mousemove', function(e){
+        if (e.buttons == 1){
+            eraseColor(e);
+            e.preventDefault();
+        }});
+    container.addEventListener('click', eraseColor);
+}
 
 function usingRandomizer(){
 
     container.addEventListener('mousemove', function(e){
         if (e.buttons == 1){
             randomColor(e);
-            e.preventDefault()
-        }
-    });
-    container.addEventListener('click', randomColor)
+            e.preventDefault();
+        }});
+    container.addEventListener('click', randomColor);
 }
+
+// function clickBtn(e){
+//     if(e.target.classList.contains("btn")){
+//         for(button in buttons)
+//         buttons.forEach(function(button){
+//         button.classList.remove("active");
+//         });
+//         e.target.classList.add("active");  
+//      };
+// };
 
              
 
 function events(){
     makeDivs();
     usingPen();
-    randomBtn.addEventListener('click', () => {usingRandomizer()})
+    randomBtn.addEventListener('click', () => {usingRandomizer()});
     penBtn.addEventListener('click',() => {usingPen()});
     eraserBtn.addEventListener('click', () => {usingEraser()});
     clearBtn.addEventListener('click', () => {clearEverything()});
     slider.addEventListener('input', () => {
         container.replaceChildren();
         makeDivs()});
+    document.addEventListener("click", function(evt){
+        if(evt.target.classList.contains("button-categories")){
+            buttons.forEach(function(button){
+                button.classList.remove("active");
+    });
+        evt.target.classList.add("active");
+  }
+});
     
 }
+
+
+
+
 
 
 
